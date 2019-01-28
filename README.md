f1909b5bfee1e0f2f27a729abf039bf640694fa4
john.bianchi <john.bianchi@polarisalpha.com>
2019-01-28 16:34:56 -0700

    add stat socket

--- README.md
+++ README.md
@@ -17,9 +17,10 @@ Use <http://localhost:9002> for the Nginx proxy'ing.
 ## Apache Benchmark
 
 ### ab test haproxy endpoint
-
+```
 $ ab -n 100000 -c 1000 -r -d http://localhost:9001/
-
+```
 ### ab test nginx endpoint
-
+```
 $ ab -n 100000 -c 1000 -r -d http://localhost:9002/
+```
--- start-env.sh
+++ start-env.sh
@@ -10,14 +10,14 @@ function info() {
   echo -e "\e[32mINFO: $@\e[0m"
 }
 
-[[ $VERBOSE ]] && info "move into directory where the script is"
+[[ -n ${VERBOSE} ]] && info "get directory where the script is running from" # caveats
 CWD="$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )"
-[[ $VERBOSE ]] && info "CWD: ${CWD}"
+[[ -n ${VERBOSE} ]] && info "CWD: ${CWD}"
 
 
-[[ $VERBOSE ]] && info "define NETWORK_NAME"
+[[ -n ${VERBOSE} ]] && info "define NETWORK_NAME"
 NETWORK_NAME="test-lan"
-[[ $VERBOSE ]] && info "NETWORK_NAME: ${NETWORK_NAME}"
+[[ -n ${VERBOSE} ]] && info "NETWORK_NAME: ${NETWORK_NAME}"
 if [[ $(docker network ls | grep ${NETWORK_NAME}) ]]; then
   info "found docker network: ${NETWORK_NAME}"
 else
@@ -25,14 +25,18 @@ else
   ${DRYRUN:+echo} docker network create ${NETWORK_NAME}
 fi
 
-info 'stand up three httpd hosts, httpd-01 - httpd-03'
-for host in 01 02 03; do
-  info "creating httpd-${host}"
-  ${DRYRUN:+echo} docker run -dit --name=httpd-${host} --net=${NETWORK_NAME} --log-driver=json-file --user=root -v "$CWD/html/":/usr/local/apache2/htdocs/:rw httpd:2.4-alpine
+info 'stand up three httpd nodes, httpd-01 - httpd-03'
+for node in 01 02 03; do
+  info "creating httpd-${node}"
+  ${DRYRUN:+echo} mkdir -p ${VERBOSE+ -v} html/${node}
+  # if not dryrun, write to the node file
+  [[ -n ${VERBOSE} ]] && info "creating node index.html"
+  [[ -z ${DRYRUN} ]] && echo -e "Hello from httpd-${node}" > html/${node}/index.html
+  ${DRYRUN:+echo} docker run -dit --name=httpd-${node} --net=${NETWORK_NAME} --log-driver=json-file --user=root -v "${CWD}/html/${node}/":/usr/local/apache2/htdocs/:rw httpd:2.4-alpine
 done
 
 info 'stand up haproxy'
-${DRYRUN:+echo} docker run -dit --name=haproxy --log-driver=json-file --net=${NETWORK_NAME} -v "${CWD}/haproxy/":/usr/local/etc/haproxy/:rw -p 9001:80 haproxy:1.8-alpine
+${DRYRUN:+echo} docker run -dit --name=haproxy --log-driver=json-file --net=${NETWORK_NAME} -v "${CWD}/haproxy/":/usr/local/etc/haproxy/:rw -v /var/run/haproxy:/var/run/haproxy -p 9001:80 haproxy:1.8-alpine
 
 info 'stand up nginx'
 ${DRYRUN:+echo} docker run -dit --name=nginx --log-driver=json-file --net=${NETWORK_NAME} -v "${CWD}/nginx/":/etc/nginx/:rw -p 9002:80 nginx:1.15-alpine
